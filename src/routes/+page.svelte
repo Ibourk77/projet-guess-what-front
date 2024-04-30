7eab48c9d46792e611a25a5ab92dc39f1b3b567e
ibourkabdellah <141337646+Ibourk77@users.noreply.github.com>
2024-04-30 15:41:47 +0200

    modif2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,56 +1,52 @@
-
 <script>
-
   // Exportation de la variable 'data' depuis un autre module ou composant.
   export let data;
 
-// Initialisation d'un tableau 'scores' pour stocker les scores.
+  // Initialisation d'un tableau 'scores' pour stocker les scores.
   let scores = [];
 
- // Affichage de la propriété 'themes' de l'objet 'data' pour le débogage.
-  console.log(data.themes)
+  // Affichage de la propriété 'themes' de l'objet 'data' pour le débogage.
+  console.log(data.themes);
 
   // Fonction asynchrone 'handleChange' pour gérer les changements de thème.
   async function handleChange(event) {
-
-  // Affichage du thème sélectionné pour le débogage.
-    console.log('on change de thèmes', event.target.value);
+    // Affichage du thème sélectionné pour le débogage.
+    console.log("on change de thèmes", event.target.value);
 
     const themeId = event.target.value;
-    
- //  Appel à une API pour récupérer le classement par thème.
- // Utiliser HTTPS pour sécuriser la requête.
-    const result = await fetch(`https://guesswhat-api.onrender.com/theme/${themeId}/leaderboard`);
 
+    //  Appel à une API pour récupérer le classement par thème.
+    // Utiliser HTTPS pour sécuriser la requête.
+    const result = await fetch(
+      `https://guess-what-back.onrender.com/theme/${themeId}/leaderboard`
+    );
 
- //Conversion de la réponse de l'API en JSON et stockage dans 'scores'.
+    //Conversion de la réponse de l'API en JSON et stockage dans 'scores'.
     scores = await result.json();
 
-    console.log(`les scores pour le thème ${event.target.value}`, scores)
-    
+    console.log(`les scores pour le thème ${event.target.value}`, scores);
   }
 
   //  Création d'un tableau 'lignes' avec 3 objets par défaut pour l'affichage.
-   let lignes = new Array(3).fill({id: "—", score: "—", rank: "—"});
+  let lignes = new Array(3).fill({ id: "—", score: "—", rank: "—" });
 
   //  Importation de la fonction 'goto' pour la navigation.
-   import { goto } from '$app/navigation';
+  import { goto } from "$app/navigation";
 
   // Fonction 'playGame' pour naviguer vers le jeu.
   function playGame() {
-     goto('/game/visiteur'); 
-   }
-  
-  </script>
+    goto("/game/visiteur");
+  }
+</script>
 
 <main>
-   <!--  Section pour sélectionner un thème -->
-   <section id="theme-selector">
+  <!--  Section pour sélectionner un thème -->
+  <section id="theme-selector">
     <label for="theme_select">Choisissez un thème :</label>
     <select on:change={handleChange} name="theme" id="theme_select">
       <option> -- Choisissez un thème --</option>
       {#each data.themes as theme, i}
-        <option value="{theme.id}">{theme.name}</option>
+        <option value={theme.id}>{theme.name}</option>
       {/each}
     </select>
   </section>
@@ -62,7 +58,8 @@
       <thead>
         <tr>
           <th>Rang</th>
-          <th>Pseudo</th> <!-- Colonne ajoutée pour les pseudos -->
+          <th>Pseudo</th>
+          <!-- Colonne ajoutée pour les pseudos -->
           <th>Score</th>
           <th>Date</th>
         </tr>
@@ -71,37 +68,50 @@
         {#each scores as score, index}
           <tr>
             <td>{index + 1}</td>
-            <td>{score.player.username}</td> <!-- Affichage des pseudos ici -->
+            <td>{score.player.username}</td>
+            <!-- Affichage des pseudos ici -->
             <td>{score.score}</td>
-            <td>{new Date(score.createdAt).toLocaleString('fr-FR', {
-              weekday: "long",
-              year: "numeric",
-              month: "long",
-              day: "numeric",
-              hour: "numeric",
-              minute: "numeric"
-            })}</td>
+            <td
+              >{new Date(score.createdAt).toLocaleString("fr-FR", {
+                weekday: "long",
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+                hour: "numeric",
+                minute: "numeric",
+              })}</td
+            >
           </tr>
         {/each}
       </tbody>
     </table>
   </section>
 
-   <!-- Section de présentation du jeu -->
-    <h1>Devinettes pour développeur fullstack</h1>
+  <!-- Section de présentation du jeu -->
+  <h1>Devinettes pour développeur fullstack</h1>
 
-      <p>🚀 Bienvenue sur <a href="/" class="nomjeux">Guess What</a> – Le Fruit de Notre Passion pour le Développement web!
-        🚀Chers utilisateurs,Nous sommes quatre développeurs passionnés, unis par notre amour pour la technologie et les défis intellectuels. 
-        Guess What est le projet d'apothéose de notre parcours de <a href="https://oclock.io/formations/developpeur-web-fullstack-javascript" class="nomjeux">formation</a> à l'école <a href="https://oclock.io/" class="nomjeux">O'clock</a>, une célébration de nos compétences en développement et de notre créativité.
-        🧩 Découvrez des énigmes conçues avec soin, où chaque détail est un clin d'œil à l'univers du développement. 
-        👥 Vivez une aventure collaborative, inspirée par notre propre expérience en tant qu'équipe.
-        💡 Testez vos connaissances sur le HTML, CSS & JAVASCRIPT, affûtez votre logique et partagez le plaisir de résoudre des devinettes captivantes.
-        Ce jeu est notre rêve devenu réalité, une invitation à explorer le monde fascinant du développement à travers le prisme ludique des devinettes.
-         Prêts à coder votre chemin à travers les énigmes? Que l'aventure commence!"</p>
-        
-   <!-- Bouton pour démarrer le jeu -->
-      <button id="playButton" on:click={playGame}>Jouer</button> 
-      
-    
-  </main>
-  
+  <p>
+    🚀 Bienvenue sur <a href="/" class="nomjeux">Guess What</a> – Le Fruit de
+    Notre Passion pour le Développement web! 🚀Chers utilisateurs,Nous sommes
+    quatre développeurs passionnés, unis par notre amour pour la technologie et
+    les défis intellectuels. Guess What est le projet d'apothéose de notre
+    parcours de
+    <a
+      href="https://oclock.io/formations/developpeur-web-fullstack-javascript"
+      class="nomjeux">formation</a
+    >
+    à l'école <a href="https://oclock.io/" class="nomjeux">O'clock</a>, une
+    célébration de nos compétences en développement et de notre créativité. 🧩
+    Découvrez des énigmes conçues avec soin, où chaque détail est un clin d'œil
+    à l'univers du développement. 👥 Vivez une aventure collaborative, inspirée
+    par notre propre expérience en tant qu'équipe. 💡 Testez vos connaissances
+    sur le HTML, CSS & JAVASCRIPT, affûtez votre logique et partagez le plaisir
+    de résoudre des devinettes captivantes. Ce jeu est notre rêve devenu
+    réalité, une invitation à explorer le monde fascinant du développement à
+    travers le prisme ludique des devinettes. Prêts à coder votre chemin à
+    travers les énigmes? Que l'aventure commence!"
+  </p>
+
+  <!-- Bouton pour démarrer le jeu -->
+  <button id="playButton" on:click={playGame}>Jouer</button>
+</main>
--- src/routes/game/connecter/+page.svelte
+++ src/routes/game/connecter/+page.svelte
@@ -1,15 +1,15 @@
 <script>
   // Exportation de la variable 'data', qui permet de recevoir des données depuis le composant parent.
   export let data;
-  
+
   // Affichage du token pour le débogage.
-  console.log(data.token)
+  console.log(data.token);
 
   // Importation du hook 'onMount' depuis 'svelte', utilisé pour exécuter du code une fois que le composant est monté.
-  import { onMount } from 'svelte';
-  let selectedTheme = 'default'; // Thème sélectionné par défaut.
+  import { onMount } from "svelte";
+  let selectedTheme = "default"; // Thème sélectionné par défaut.
   let themes = data.themes; // Liste des thèmes récupérée depuis 'data'.
-  let hint = ''; // Indice pour la question actuelle.
+  let hint = ""; // Indice pour la question actuelle.
   let questions = []; // Liste des questions pour le thème sélectionné.
   let themeScores = {}; // Scores par thème.
   let themeHintsUsed = {}; // Compteur d'indices utilisés par thème.
@@ -17,7 +17,7 @@
 
   // Initialisation des scores, indices utilisés et mauvaises réponses pour chaque thème lors du montage du composant.
   onMount(async () => {
-    themes.forEach(theme => {
+    themes.forEach((theme) => {
       themeScores[theme.id] = 0;
       themeHintsUsed[theme.id] = 0;
       themeIncorrectAnswers[theme.id] = 0;
@@ -28,12 +28,14 @@
   async function handleThemeChange(event) {
     selectedTheme = event.target.value;
     // Requête à l'API pour récupérer les questions du thème sélectionné.
-    const response = await fetch(`https://guesswhat-api.onrender.com/theme/${selectedTheme}`);
+    const response = await fetch(
+      `https://guess-what-back.onrender.com/theme/${selectedTheme}`
+    );
     if (response.ok) {
       const data = await response.json();
-      questions = data.riddles.map(question => ({
+      questions = data.riddles.map((question) => ({
         ...question,
-        usedHint: false // Ajout d'un indicateur pour chaque question pour savoir si un indice a été utilisé.
+        usedHint: false, // Ajout d'un indicateur pour chaque question pour savoir si un indice a été utilisé.
       }));
     }
   }
@@ -41,27 +43,31 @@
   // Fonction pour demander un indice pour une question spécifique.
   function requestHint(questionId) {
     hint = "Voici un indice pour la question.";
-    const questionIndex = questions.findIndex(q => q.id === questionId);
+    const questionIndex = questions.findIndex((q) => q.id === questionId);
     if (questionIndex !== -1) {
       questions[questionIndex].usedHint = true; // Marque que l'indice a été utilisé pour cette question.
       themeHintsUsed[selectedTheme]++;
-      hint = questions[questionIndex].indicator // Mise à jour de l'indice avec l'information spécifique à la question.
+      hint = questions[questionIndex].indicator; // Mise à jour de l'indice avec l'information spécifique à la question.
     }
   }
 
   // Fonction pour vérifier la réponse à une question.
   async function checkAnswer(answer, questionId) {
-    const questionIndex = questions.findIndex(q => q.id === questionId);
+    const questionIndex = questions.findIndex((q) => q.id === questionId);
     if (questionIndex !== -1) {
       if (answer.is_good_answer) {
         // Calcul du score avec une pénalité si un indice a été utilisé.
-        themeScores[selectedTheme] += questions[questionIndex].usedHint ? 1.5 : 2;
+        themeScores[selectedTheme] += questions[questionIndex].usedHint
+          ? 1.5
+          : 2;
       } else {
         themeIncorrectAnswers[selectedTheme]++; // Incrémentation du compteur de mauvaises réponses.
       }
       // Désactivation des boutons de réponse pour cette question.
-      const btnAnswers = document.querySelectorAll(`#answers-${questionId} button`);
-      btnAnswers.forEach(btn => btn.setAttribute('disabled', true));
+      const btnAnswers = document.querySelectorAll(
+        `#answers-${questionId} button`
+      );
+      btnAnswers.forEach((btn) => btn.setAttribute("disabled", true));
     }
   }
 
@@ -71,43 +77,52 @@
       theme_id: themeId,
       score: score,
       count_indicators: hintsUsed,
-      errors: incorrectAnswers
+      errors: incorrectAnswers,
     };
     try {
       // Envoi des données de score à l'API.
-      const response = await fetch('https://guesswhat-api.onrender.com/user/play', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          "Authorization": "Bearer " + data.token
-        },
-        body: JSON.stringify(scoreData)
-      });
+      const response = await fetch(
+        "https://guesswhat-api.onrender.com/user/play",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + data.token,
+          },
+          body: JSON.stringify(scoreData),
+        }
+      );
       if (!response.ok) {
-        throw new Error('Erreur lors de la mise à jour du score');
+        throw new Error("Erreur lors de la mise à jour du score");
       }
       // Traitement supplémentaire après la mise à jour du score.
     } catch (error) {
-      console.error('Erreur lors de l’envoi du score:', error);
+      console.error("Erreur lors de l’envoi du score:", error);
     }
   }
 </script>
 
-
 <main>
   <div class="top-section">
     <div class="score-section">
-      <span>Scores pour {themes.find(t => t.id === selectedTheme)?.name || 'le thème sélectionné'} :</span>
+      <span
+        >Scores pour {themes.find((t) => t.id === selectedTheme)?.name ||
+          "le thème sélectionné"} :</span
+      >
       <div class="score-box">
         {themeScores[selectedTheme] || 0}
       </div>
     </div>
     <div class="theme-section">
       <label for="theme-select">Choix du thème :</label>
-      <select id="theme-select" bind:value={selectedTheme} on:change={handleThemeChange}>
+      <select
+        id="theme-select"
+        bind:value={selectedTheme}
+        on:change={handleThemeChange}
+      >
         <option> -- Choisissez un thème --</option>
         {#each themes as theme}
-          <option value="{theme.id}">{theme.name}</option>
+          <option value={theme.id}>{theme.name}</option>
         {/each}
       </select>
     </div>
@@ -120,32 +135,49 @@
       </div>
     </div>
     <div class="hint-section">
-      <button on:click={() => requestHint(question.id)}>Veux-tu un indice ?</button>
+      <button on:click={() => requestHint(question.id)}
+        >Veux-tu un indice ?</button
+      >
       {#if hint === question.indicator}
         <div class="hint-box">{hint}</div>
-      {/if} 
-      
+      {/if}
     </div>
     <div id={`answers-${question.id}`} class="answers-section">
       {#each question.answers as answer}
-        <button on:click={() => checkAnswer(answer, question.id)}>{answer.content}</button>
+        <button on:click={() => checkAnswer(answer, question.id)}
+          >{answer.content}</button
+        >
       {/each}
     </div>
   {/each}
 
-  <button on:click={() => updateScore(selectedTheme, themeScores[selectedTheme], themeHintsUsed[selectedTheme], themeIncorrectAnswers[selectedTheme])}>valider</button>
+  <button
+    on:click={() =>
+      updateScore(
+        selectedTheme,
+        themeScores[selectedTheme],
+        themeHintsUsed[selectedTheme],
+        themeIncorrectAnswers[selectedTheme]
+      )}>valider</button
+  >
 </main>
 
 <style>
-  .top-section, .hint-section, .answers-section {
+  .top-section,
+  .hint-section,
+  .answers-section {
     margin-bottom: 20px;
   }
 
-  .theme-section, .question-section, .score-section {
+  .theme-section,
+  .question-section,
+  .score-section {
     margin-bottom: 10px;
   }
 
-  .question-box, .score-box, .hint-box {
+  .question-box,
+  .score-box,
+  .hint-box {
     border: 1px solid #ccc;
     padding: 10px;
     margin-top: 5px;
@@ -154,13 +186,12 @@
   .answers-section button {
     padding: 10px;
     margin: 5px;
-    background-color: #677A91;
-    color: #FFCD29;
+    background-color: #677a91;
+    color: #ffcd29;
     border: none;
     cursor: pointer;
   }
   .answers-section button:hover {
     opacity: 0.8;
   }
-
 </style>
